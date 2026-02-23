c19151ce2621ffd6f719900e390f81802842ac90
Kacper Barabasz <barabasz64@gmail.com>
2026-02-23 20:51:44 +0100

    Enhance styling in index.css and add controls to ChartSlider component for improved user interaction.

--- src/components/ChartSlider.tsx
+++ src/components/ChartSlider.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import RevenueByCountryChart from './RevenueByCountryChart';
 
 import styles from './ChartSlider.module.css';
 
+import data from '../../data/data.json' with { type: 'json' };
+
 const ChartSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  console.log(data);
   return (
     <div className={styles.slider}>
       <div className={styles.slide}>
         <RevenueByCountryChart />
+      </div>
+
+      <div className={styles.controls}>
+        <button className={styles.btn}>Previous</button>
+        <div className={styles.dots}>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+        </div>
+        <button className={styles.btn}>Next</button>
       </div>
     </div>
   );
